ba3e7009a9dbb13937b79c137b86e755277c30e5
acoulton <andrew@ingenerator.com>
2025-04-08 08:35:15 +0100

    Remove "Show Source" buttons & configure "Edit on Github"
    I don't think we need the clutter of a separate "Show Source" if
    we have the (much more useful) "Edit on Github" button.

--- conf.py
+++ conf.py
@@ -29,9 +29,16 @@ htmlhelp_basename = 'behat'
 
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 html_theme='sphinx_wagtail_theme'
+html_theme_options = dict(
+  # Base path for "Edit on Github" links
+  github_url="https://github.com/Behat/docs/blob/v3.0/",
+)
 
 # If false, no index is generated.
 # Since we are implementing search with Algolia DocSearch through ReadTheDocs,
 # we do not need Sphinx to generate its own index. It might not hurt to keep
 # the Sphinx index, but speeds up the build process.
 html_use_index = False
+
+# Don't show separate "view source" links on generated pages
+html_show_sourcelink = False
